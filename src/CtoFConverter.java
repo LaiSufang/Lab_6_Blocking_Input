03402773af2d75d36dd63aac1f50b6c54ccdd410
Sufang Lai <laisg@mail.uc.edu>
2024-06-16 20:36:21 -0400

    updated do-while loop

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -4,22 +4,26 @@ public class CtoFConverter {
     public static void main(String[] args) {
         //A program that converts an inputted temperature in C and provides the equivalent temperature in F.
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        double tempC = 0;
         double tempF;
-        double userInput;
+        String badInput;
         boolean validInput = false;
 
         do {
-            System.out.println("Enter a temperature in Celsius: ");
-            while (!scanner.hasNextDouble()) {
-                System.out.println("Invalid input. Please enter a valid temperature in Celsius: ");
-                scanner.next();
+            System.out.println("Please enter a temperature in Celsius: ");
+            if (in.hasNextDouble()) { //checks if the input is a double
+                tempC = in.nextDouble(); //stores the input in tempC
+                in.nextLine(); //clears the buffer
+                validInput = true; //exits the loop
             }
-            userInput = scanner.nextDouble();
-        }
-        while (false);
+            else {
+                badInput = in.nextLine(); //stores the invalid input
+                System.out.println(badInput + " is not a valid temperature input.");
+            }
+        } while (!validInput);
 
-        tempF = (userInput * 9/5) + 32;
+        tempF = (tempC * 9/5) + 32;
         System.out.println("The temperature in Fahrenheit is: " + tempF);
     }
 }
